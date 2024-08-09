adfef43f1f1230b950e74e675f4d2c6f3bb13069
Varga Tamas <vargata1@gmail.com>
2024-08-10 00:06:18 +0200

    design modification for authenticated user

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,17 +1,67 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
+<style>
+    html, body {
+        height: 100%;
+        margin: 0;
+    }
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
-                </div>
-            </div>
+    .centered-box {
+        width: 50%;
+        height: 50%;
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        background-color: white;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        text-align: justify;
+        box-sizing: border-box;
+        padding: 20px;
+        font-size: 25px;
+    }
+
+    .centered-box ul {
+        list-style-type: none;
+        padding: 0;
+        margin: 0;
+    }
+
+    .centered-box li {
+        margin-bottom: 10px;
+    }
+
+    .centered-box a {
+        color: black;
+        text-decoration: none;
+        font-weight: bold;
+    }
+
+    .centered-box a:hover {
+        text-decoration: underline;
+    }
+
+    @media (max-width: 600px) {
+        .centered-box {
+            width: 80%;
+            height: 60%;
+        }
+    }
+
+</style>
+<x-app-layout>
+    @section('content')
+        <div class="centered-box">
+            <ul>
+                <li><a href="/api/documentation" target="_blank">NASA API</a></li>
+                <li><a href="https://www.nasa.gov/nasalive" target="_blank">NASA TV - Official Stream</a></li>
+                <li><a href="https://hubblesite.org/images/gallery" target="_blank">Hubble Space Telescope - Image Gallery</a></li>
+                <li><a href="https://apod.nasa.gov/apod/" target="_blank">Astronomy Picture of the Day</a></li>
+                <li><a href="https://www.n2yo.com/?s=25544" target="_blank">Track the ISS</a></li>
+                <li><a href="https://mars.nasa.gov/mars2020/" target="_blank">Perseverance Rover on Mars</a></li>
+                <li><a href="https://eyes.nasa.gov/" target="_blank">Explore with NASA's Eyes</a></li>
+            </ul>
         </div>
-    </div>
+    @endsection
+
 </x-app-layout>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -860,7 +860,7 @@
     </style>
 </head>
 <body class="font-sans antialiased dark:bg-black dark:text-white/50">
-<div class="bg-gray-50 text-black/50 dark:bg-black dark:text-white/50">
+<div class="text-black/50 dark:bg-black dark:text-white/50">
     <div
         class="relative min-h-screen flex flex-col items-center justify-center selection:bg-[#FF2D20] selection:text-white">
         <div class="relative w-full max-w-2xl px-6 lg:max-w-7xl">
