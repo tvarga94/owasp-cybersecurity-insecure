85a34ff4a7e4c5b16fcf32c2a0f6a631540672b4
tvarga94 <vargata1@gmail.com>
2025-08-09 10:54:09 +0200

    SSRF attack

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AdminController;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('throttle:10,1')->group(function () {
@@ -21,3 +22,25 @@ Route::put('/admin/users/{user}', [AdminController::class, 'update'])->name('adm
 Route::delete('/admin/users/{user}', [AdminController::class, 'destroy'])->name('admin.users.destroy');
 
 require __DIR__.'/auth.php';
+
+Route::get('/preview-unsafe', function (Request $request) {
+    $url = $request->query('url');
+
+    if (!$url) {
+        return '<form method="GET">
+                    <label>Enter a URL to preview:</label><br>
+                    <input type="text" name="url" style="width:300px;" placeholder="https://example.com">
+                    <button type="submit">Preview</button>
+                </form>';
+    }
+
+    $content = file_get_contents($url);
+    return "<h3>Preview of $url:</h3><pre>" . e(substr($content, 0, 500)) . "</pre>";
+});
+
+Route::get('/admin-only', function (Request $request) {
+    if (!in_array($request->ip(), ['127.0.0.1', '::1'])) {
+        abort(403);
+    }
+    return 'Super secret admin panel';
+});
